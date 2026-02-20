77487b86fdcfff3943067cacb4e426e58764963f
Indresh-Gupta <indreshgupta125@gmail.com>
2026-02-20 20:27:24 +0530

    backend url is added

--- Frontend/src/Api/axios.jsx
+++ Frontend/src/Api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://employee-management-frontend-wo1t.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
